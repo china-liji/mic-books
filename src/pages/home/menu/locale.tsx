fefdc80a045db443e2ed273725ba9568abd304f3
china-liji <hello_zaizi@163.com>
2020-08-07 16:58:12 +0800

    隐藏部分菜单

--- src/pages/home/menu/locale.tsx
+++ src/pages/home/menu/locale.tsx
@@ -48,47 +48,47 @@ export const renderGroups = (language: Language, onItemSelected: MenuItemProps['
 
 export const getConfigs = ({ menu, text }: Language): Config[] => {
   return [
-    new Config(
-      'useState',
-      'use-state',
-      async (): Promise<Export> => {
-        return await import('@/demos/react/hooks/use-state') as unknown as Export;
-      },
-      'React',
-      ['hooks']
-    ),
-    new Config(
-      'useRef',
-      'use-ref',
-      async (): Promise<Export> => {
-        return await import('@/demos/react/hooks/use-ref') as unknown as Export;
-      }
-    ),
-    new Config(
-      'useCallback',
-      'use-callback',
-      async (): Promise<Export> => {
-        return await import('@/demos/react/hooks/use-callback') as unknown as Export;
-      }
-    ),
-    new Config(
-      menu.typescript.interfaceAndType,
-      'interface-vs-type',
-      async (): Promise<Export> => {
-        return await import('@/demos/typescript/interface-vs-type.md') as unknown as Export;
-      },
-      'TypeScript',
-      [PrimaryTag.Vs]
-    ),
-    new Config(
-      menu.dom.node,
-      'doc',
-      async (): Promise<Export> => {
-        return await import('@/demos/dom/doc') as unknown as Export;
-      },
-      'DOM',
-      [PrimaryTag.Knowledge]
-    ),
+    // new Config(
+    //   'useState',
+    //   'use-state',
+    //   async (): Promise<Export> => {
+    //     return await import('@/demos/react/hooks/use-state') as unknown as Export;
+    //   },
+    //   'React',
+    //   ['hooks']
+    // ),
+    // new Config(
+    //   'useRef',
+    //   'use-ref',
+    //   async (): Promise<Export> => {
+    //     return await import('@/demos/react/hooks/use-ref') as unknown as Export;
+    //   }
+    // ),
+    // new Config(
+    //   'useCallback',
+    //   'use-callback',
+    //   async (): Promise<Export> => {
+    //     return await import('@/demos/react/hooks/use-callback') as unknown as Export;
+    //   }
+    // ),
+    // new Config(
+    //   menu.typescript.interfaceAndType,
+    //   'interface-vs-type',
+    //   async (): Promise<Export> => {
+    //     return await import('@/demos/typescript/interface-vs-type.md') as unknown as Export;
+    //   },
+    //   'TypeScript',
+    //   [PrimaryTag.Vs]
+    // ),
+    // new Config(
+    //   menu.dom.node,
+    //   'doc',
+    //   async (): Promise<Export> => {
+    //     return await import('@/demos/dom/doc') as unknown as Export;
+    //   },
+    //   'DOM',
+    //   [PrimaryTag.Knowledge]
+    // ),
     new Config(
       text.introduction,
       'readme',
